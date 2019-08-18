1f76b5fded226b760bee22f983292575fe02a33b
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-18 17:06:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,12 @@ def countdown(int)
   return "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(int)
+  count = int 
+  while count > 0 
+    puts "#{count} SECOND(S)!"
+    count -= 1 
+  end
+  return "HAPPY NEW YEAR!"
+end
+
