f41e818ab9f3b4647dbf6fec28f0ddbf1d5b8a6a
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-18 17:05:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  count = int 
+  while count > 0 
+    puts "#{count} SECOND(S)!"
+    count -= 1 
+  end
+  return "HAPPY NEW YEAR!"
 end
